cc0a7655bc2395bf8681215117b75f33b48d0e8c
lxy-yz <lxy.dev.yz@gmail.com>
2023-04-02 23:32:43 -0700

    chore: input placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -334,7 +334,7 @@ export default function Home() {
                   ref={inputRef}
                   className="h-12 w-full rounded-full border border-zinc-600 pr-12 pl-11 focus:border-zinc-800 focus:outline-none focus:ring-1 focus:ring-zinc-800 sm:h-16 sm:py-2 sm:pr-16 sm:pl-16 sm:text-lg"
                   type="text"
-                  placeholder="Ask me anything ..."
+                  placeholder="Ask allen anything ..."
                   value={query}
                   onChange={(e) => setQuery(e.target.value)}
                   onKeyDown={handleKeyDown}
