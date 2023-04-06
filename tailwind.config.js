384f6dbcab82de30f625afb417a4dcbac31657f9
lxy-yz <lxy.dev.yz@gmail.com>
2023-04-05 22:30:07 -0700

    fix: local background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        gpt: "url('https://img.freepik.com/free-vector/abstract-blue-circle-black-background-technology_1142-12714.jpg?w=2000&t=st=1680551223~exp=1680551823~hmac=0c9b905c631b40bf68606df33cc2082bd81629c9a1f7a126eaecc43107b6d6dd')",
+        gpt: "url('/bg.jpeg')",
       },
     },
   },
