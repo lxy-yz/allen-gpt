d2c6ac42f9fbecd1eb90f74538ef551b7de8a09e
lxy-yz <lxy.dev.yz@gmail.com>
2023-04-08 22:03:28 -0700

    feat: use https://www.vantajs.com

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,42 @@
 import "@/styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
+
+declare global {
+  interface Window {
+    VANTA: any;
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps<{}>) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
+
+      <div>
+        <div id="background" className='w-screen h-screen !absolute -z-10 top-0 left-0'>
+        </div>
+        <style>{`
+          .vanta-canvas {
+            width: 100% !important;
+            height: 100% !important;
+          }
+        `}</style>
+
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></Script>
+        <Script
+          src="https://cdn.jsdelivr.net/npm/vanta/dist/vanta.net.min.js"
+          onReady={() => {
+            window.VANTA.NET({
+              el: '#background',
+              color: '#06b6d4'
+            })
+          }}
+        />
+      </div>
     </main>
   );
 }
